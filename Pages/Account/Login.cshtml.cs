355dafff9ece4b3684c8739a3ccffa98436c601d
Emerson <emerson@gmail.com>
2025-05-13 23:13:32 -0500

    Autorizacion

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Autenticacion.Model;
 using Microsoft.Identity.Client.Platforms.Features.DesktopOs;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 
 namespace Autenticacion.Pages.Account
 {
@@ -14,8 +16,27 @@ namespace Autenticacion.Pages.Account
         {
         }
 
-        public void OnPost() {
-            Console.WriteLine("User: " + User.Email + "Password : " + User.Password);
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid) return Page();
+
+            if (User.Email == "correo@gmail.com" && User.Password == "12345")
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email,User.Email),
+                };
+
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+                return RedirectToPage("/Index");
+            }
+            return Page();
         }
+        
     }
 }
